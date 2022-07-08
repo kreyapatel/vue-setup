0384f808decfc7730d75476a24be4da226a00b1b
Kakadiya Ravina <kakadiyamk11@gmail.com>
2022-07-08 15:42:23 +0530

    Sidebar changes 8-7-2022

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,13 +1,17 @@
 <template>
   <v-container>
-    <h1>Home Page</h1>
+    <!-- <h1>Home Page</h1> -->
+    <SidebarComponent/>
   </v-container>
 </template>
 
 <script>
+  import SidebarComponent from'@/components/SidebarComponent.vue'
   export default {
     name: 'HomePage',
-
+    components:{
+      SidebarComponent,
+    },
     data: () => ({
     }),
   }
